177c8f9fe2ee3d9c3a6374791ed8fa3826866917
kushwahaPrashant24 <146343202+kushwahaPrashant24@users.noreply.github.com>
2024-05-12 17:39:39 +0530

    completed navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,51 @@
 import { useState } from 'react'
 import './App.css'
+import ResponsiveAppBar from './Componont/Navbar/Nav'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from '../src/Componont/Home/Home';
+import Careers from '../src/Componont/Careers/Careers';
+import Contact from '../src/Componont/Contact/Contact';
+
+
+const router = createBrowserRouter([
+  
+  {
+    path: "/Home",
+    element:<><ResponsiveAppBar/><Home/></>
+  },
+
+  {
+    path: "Services",
+    element:<><ResponsiveAppBar/><Home/></>
+  },
+
+  {
+    path: "Careers",
+    element:<><ResponsiveAppBar/><Careers/></>
+  },
+
+  {
+    path: "Contact",
+    element:<><ResponsiveAppBar/><Contact/></>
+  },
+
+ 
+
+  
+
+]);
+
 
 function App() {
   
 
   return (
     <>
-     commit
+     <RouterProvider router={router} />
+     
     </>
   )
 }
