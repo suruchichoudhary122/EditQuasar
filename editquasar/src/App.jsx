b90e98fdaee06791a5690dab5ebd334e52c4ef63
Afro Sheikh <sa8329925@gmail.com>
2024-05-13 10:40:56 +0530

    SErvice section started

--- editquasar/src/App.jsx
+++ editquasar/src/App.jsx
@@ -1,18 +1,55 @@
-import { Route, Routes } from "react-router-dom";
-import "./App.css";
-import Home from "./Components/Home"
-import Navbar from "./Components/Navbar"
+import { useState } from 'react'
+import './App.css'
+import ResponsiveAppBar from './Componont/Navbar/Nav'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from '../src/Componont/Home/Home';
+import Careers from '../src/Componont/Careers/Careers';
+import Contact from '../src/Componont/Contact/Contact';
+import Services from './Componont/Services/Services';
+
+
+const router = createBrowserRouter([
+  
+  {
+    path: "/",
+    element:<><ResponsiveAppBar/><Home/></>
+  },
+
+  {
+    path: "Services",
+    element:<><ResponsiveAppBar/><Services/></>
+  },
+
+  {
+    path: "Careers",
+    element:<><ResponsiveAppBar/><Careers/></>
+  },
+
+  {
+    path: "Contact",
+    element:<><ResponsiveAppBar/><Contact/></>
+  },
+
+ 
+
+  
+
+]);
+
 
 function App() {
+  
+
   return (
-   <div>
-    <Navbar/>
-    <Routes>
-      <Route path="/" element={<Home/>}></Route>
-      
-    </Routes>
-   </div>
-  );
+    <>
+    
+     <RouterProvider router={router} />
+     
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ editquasar/src/Componont/Services/Services.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./service.css"
+
+export default function Services() {
+    return <div className="">
+       starting
+    </div>
+} 
