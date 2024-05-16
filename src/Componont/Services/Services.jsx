8bc4e882d730bcd0b30354721fe86c8ecadfa71d
kushwahaPrashant24 <146343202+kushwahaPrashant24@users.noreply.github.com>
2024-05-16 23:04:17 +0530

    made changes

--- src/Componont/Services/Services.jsx
+++ src/Componont/Services/Services.jsx
@@ -1,8 +1,377 @@
 import React from "react";
+import "./Services.css"
+
 
 
 export default function Services() {
     return <>
-        Hi this is services section
+        <div>
+      <div>
+        {" "}
+        <p className="text-6xl bg-slate-700 py-4 text-center text-richblack-50">
+          Our Services
+        </p>
+        <div className="max-w-maxContent w-11/12 px-5 mx-auto">
+          <p className="text-xl text-center text-yellow-500 md:text-3xl">
+            "Unleash Creative Excellence with EditQuasar Your Partner for
+            Crafting Captivating Masterpieces"
+          </p>
+        </div>
+      </div>
+
+      <div className="wrapper">
+        {/* img 1  */}
+        <div className="cols">
+          <div className="col">
+            <div className="container">
+              <div className="front img1">
+                <div className="inner">
+                  <p>Photo Editing:</p>
+                  <span>
+                    Transform images with EditQuasar's professional photo
+                    editing. Meticulous attention ensures stunning results. For
+                    more samples and pricing.
+                  </span>
+                </div>
+              </div>
+              <div className="back">
+                <div className="inner">
+                  <div className="flex flex-col">
+                    <button className="bg-blue-500 mx-auto hover:bg-blue-900 text-richblack-50 font-bold py-1 px-2 rounded mb-5 transition-transform duration-300 transform hover:-translate-y-2">
+                      Explore Our Work
+                    </button>
+                    <button className="bg-blue-500 mx-auto hover:bg-blue-900 text-richblack-50 font-bold py-1 px-2 rounded mb-5 transition-transform duration-300 transform hover:-translate-y-2">
+                      View Pricing
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/* img 2  */}
+          <div className="col">
+            <div className="container">
+              <div className="front img2">
+                <div className="inner">
+                  <p>Logo Designing:</p>
+                  <span>
+                    Make a memorable impression with EditQuasar's meaningful
+                    logo design. Our creative team ensures a logo that
+                    resonates. .
+                  </span>
+                </div>
+              </div>
+              <div className="back">
+                <div className="inner">
+                  <div className="flex flex-col">
+                    <button className="bg-blue-500 mx-auto hover:bg-blue-900 text-richblack-50 font-bold py-1 px-2 rounded mb-5 transition-transform duration-300 transform hover:-translate-y-2">
+                      Explore Our Work
+                    </button>
+                    <button className="bg-blue-500 mx-auto hover:bg-blue-900 text-richblack-50 font-bold py-1 px-2 rounded mb-5 transition-transform duration-300 transform hover:-translate-y-2">
+                      View Pricing
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* img 3 */}
+          <div className="col">
+            <div className="container">
+              <div className="front img3">
+                <div className="inner">
+                  <p>Website Designing:</p>
+                  <span>
+                    Create a seamless online experience with EditQuasar's
+                    website designing. Modern, responsive, engaging websites
+                    that reflect your brand
+                  </span>
+                </div>
+              </div>
+              <div className="back">
+                <div className="inner">
+                  <div className="flex flex-col">
+                    <button className="bg-blue-500 mx-auto hover:bg-blue-900 text-richblack-50 font-bold py-1 px-2 rounded mb-5 transition-transform duration-300 transform hover:-translate-y-2">
+                      Explore Our Work
+                    </button>
+                    <button className="bg-blue-500 mx-auto hover:bg-blue-900 text-richblack-50 font-bold py-1 px-2 rounded mb-5 transition-transform duration-300 transform hover:-translate-y-2">
+                      View Pricing
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* img 4 */}
+          <div className="col">
+            <div className="container">
+              <div className="front img4">
+                <div className="inner">
+                  <p>Blog Writing:</p>
+                  <span>
+                    Fuel your digital presence with EditPro’s dynamic blog
+                    writing. Compelling, informative content tailored to your
+                    audience.{" "}
+                  </span>
+                </div>
+              </div>
+              <div className="back">
+                <div className="inner">
+                  <div className="flex flex-col">
+                    <button className="bg-blue-500 mx-auto hover:bg-blue-900 text-richblack-50 font-bold py-1 px-2 rounded mb-5 transition-transform duration-300 transform hover:-translate-y-2">
+                      Explore Our Work
+                    </button>
+                    <button className="bg-blue-500 mx-auto hover:bg-blue-900 text-richblack-50 font-bold py-1 px-2 rounded mb-5 transition-transform duration-300 transform hover:-translate-y-2">
+                      View Pricing
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* img 5  */}
+          <div className="col">
+            <div className="container">
+              <div className="front img5">
+                <div className="inner">
+                  <p>Video Editing:</p>
+                  <span>
+                    Elevate your storytelling with EditPro’s video editing. Our
+                    editors blend footage, add effects, ensuring captivating
+                    content.{" "}
+                  </span>
+                </div>
+              </div>
+              <div className="back">
+                <div className="inner">
+                  <div className="flex flex-col">
+                    <button className="bg-blue-500 mx-auto hover:bg-blue-900 text-richblack-50 font-bold py-1 px-2 rounded mb-5 transition-transform duration-300 transform hover:-translate-y-2">
+                      Explore Our Work
+                    </button>
+                    <button className="bg-blue-500 mx-auto hover:bg-blue-900 text-richblack-50 font-bold py-1 px-2 rounded mb-5 transition-transform duration-300 transform hover:-translate-y-2">
+                      View Pricing
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/* img 6 */}
+          <div className="col">
+            <div className="container">
+              <div className="front img6">
+                <div className="inner">
+                  <p> Content Writing:</p>
+                  <span>
+                    Drive results with EditQuasar's content writing. Engaging
+                    narratives that enhance your brand's visibility.
+                  </span>
+                </div>
+              </div>
+              <div className="back">
+                <div className="inner">
+                  <div className="flex flex-col">
+                    <button className="bg-blue-500 mx-auto hover:bg-blue-900 text-richblack-50 font-bold py-1 px-2 rounded mb-5 transition-transform duration-300 transform hover:-translate-y-2">
+                      Explore Our Work
+                    </button>
+                    <button className="bg-blue-500 mx-auto hover:bg-blue-900 text-richblack-50 font-bold py-1 px-2 rounded mb-5 transition-transform duration-300 transform hover:-translate-y-2">
+                      View Pricing
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* img 7 */}
+          <div className="col">
+            <div className="container">
+              <div className="front img7">
+                <div className="inner">
+                  <p>CV/Resume Making:</p>
+                  <span>
+                    Land your dream job with EditQuasar's professionally crafted
+                    CVs/resumes. Showcase skills and achievements
+                  </span>
+                </div>
+              </div>
+              <div className="back">
+                {" "}
+                <div className="inner">
+                  <div className="flex flex-col">
+                    <button className="bg-blue-500 mx-auto hover:bg-blue-900 text-richblack-50 font-bold py-1 px-2 rounded mb-5 transition-transform duration-300 transform hover:-translate-y-2">
+                      Explore Our Work
+                    </button>
+                    <button className="bg-blue-500 mx-auto hover:bg-blue-900 text-richblack-50 font-bold py-1 px-2 rounded mb-5 transition-transform duration-300 transform hover:-translate-y-2">
+                      View Pricing
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* img 8  */}
+          <div className="col">
+            <div className="container">
+              <div className="front img8">
+                <div className="inner">
+                  <p>Personalized Advertising Poster Creation:</p>
+                  <span>
+                    Convey messages effectively with EditQuasar's advertising
+                    poster creation. Eye-catching designs that leave a lasting
+                    impact
+                  </span>
+                </div>
+              </div>
+              <div className="back">
+                <div className="inner">
+                  <div className="flex flex-col">
+                    <button className="bg-blue-500 mx-auto hover:bg-blue-900 text-richblack-50 font-bold py-1 px-2 rounded mb-5 transition-transform duration-300 transform hover:-translate-y-2">
+                      Explore Our Work
+                    </button>
+                    <button className="bg-blue-500 mx-auto hover:bg-blue-900 text-richblack-50 font-bold py-1 px-2 rounded mb-5 transition-transform duration-300 transform hover:-translate-y-2">
+                      View Pricing
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* img 9  */}
+          <div className="col">
+            <div className="container">
+              <div className="front img9">
+                <div className="inner">
+                  <p>Personal Portfolio Creation:</p>
+                  <span>
+                    Highlight talents with EditQuasar's personal portfolio
+                    creation. Showcase skills in a visually appealing manner.
+                  </span>
+                </div>
+              </div>
+              <div className="back">
+                <div className="inner">
+                  <div className="flex flex-col">
+                    <button className="bg-blue-500 mx-auto hover:bg-blue-900 text-richblack-50 font-bold py-1 px-2 rounded mb-5 transition-transform duration-300 transform hover:-translate-y-2">
+                      Explore Our Work
+                    </button>
+                    <button className="bg-blue-500 mx-auto hover:bg-blue-900 text-richblack-50 font-bold py-1 px-2 rounded mb-5 transition-transform duration-300 transform hover:-translate-y-2">
+                      View Pricing
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* img 10 */}
+          <div className="col">
+            <div className="container">
+              <div className="front img10">
+                <div className="inner">
+                  <p>Personalized Occasion Card Making:</p>
+                  <span>
+                    Celebrate moments with EditQuasar's personalized occasion
+                    card making. Custom cards that reflect event uniqueness
+                  </span>
+                </div>
+              </div>
+              <div className="back">
+                <div className="inner">
+                  <div className="flex flex-col">
+                    <button className="bg-blue-500 mx-auto hover:bg-blue-900 text-richblack-50 font-bold py-1 px-2 rounded mb-5 transition-transform duration-300 transform hover:-translate-y-2">
+                      Explore Our Work
+                    </button>
+                    <button className="bg-blue-500 mx-auto hover:bg-blue-900 text-richblack-50 font-bold py-1 px-2 rounded mb-5 transition-transform duration-300 transform hover:-translate-y-2">
+                      View Pricing
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* img 11 */}
+          <div className="col">
+            <div className="container">
+              <div className="front img11">
+                <div className="inner">
+                  <p>Company Portfolio Making::</p>
+                  <span>
+                    mpress with EditQuasar's comprehensive company portfolio.
+                    Highlight strengths and achievements professionally.
+                  </span>
+                </div>
+              </div>
+              <div className="back">
+                <div className="inner">
+                  <div className="flex flex-col">
+                    <button className="bg-blue-500 mx-auto hover:bg-blue-900 text-richblack-50 font-bold py-1 px-2 rounded mb-5 transition-transform duration-300 transform hover:-translate-y-2">
+                      Explore Our Work
+                    </button>
+                    <button className="bg-blue-500 mx-auto hover:bg-blue-900 text-richblack-50 font-bold py-1 px-2 rounded mb-5 transition-transform duration-300 transform hover:-translate-y-2">
+                      View Pricing
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* img 12 */}
+          <div className="col">
+            <div className="container">
+              <div className="front img12">
+                <div className="inner">
+                  <p>Personalized Business Card Making:</p>
+                  <span>
+                    Stand out with EditQuasar's personalized business card
+                    making. Unique, professional cards that leave a lasting
+                    impression
+                  </span>
+                </div>
+              </div>
+              <div className="back">
+                <div className="inner">
+                  <div className="flex flex-col">
+                    <button className="bg-blue-500 mx-auto hover:bg-blue-900 text-richblack-50 font-bold py-1 px-2 rounded mb-5 transition-transform duration-300 transform hover:-translate-y-2">
+                      Explore Our Work
+                    </button>
+                    <button className="bg-blue-500 mx-auto hover:bg-blue-900 text-richblack-50 font-bold py-1 px-2 rounded mb-5 transition-transform duration-300 transform hover:-translate-y-2">
+                      View Pricing
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* img 13 */}
+          <div className="col">
+            <div className="container">
+              <div className="front img13">
+                <div className="inner">
+                  <p>Background Music Making:</p>
+                  <span>
+                    Enhance content atmosphere with EditQuasar's custom
+                    background music. Original, impactful compositions tailored
+                    to your need
+                  </span>
+                </div>
+              </div>
+              <div className="back">
+                <div className="inner">
+                  <div className="flex flex-col">
+                    <button className="bg-blue-500 mx-auto hover:bg-blue-900 text-richblack-50 font-bold py-1 px-2 rounded mb-5 transition-transform duration-300 transform hover:-translate-y-2">
+                      Explore Our Work
+                    </button>
+                    <button className="bg-blue-500 mx-auto hover:bg-blue-900 text-richblack-50 font-bold py-1 px-2 rounded mb-5 transition-transform duration-300 transform hover:-translate-y-2">
+                      View Pricing
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
     </>
 } 
