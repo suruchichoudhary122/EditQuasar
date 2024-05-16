961d9797044f42786c81276fdfa842c8bfec1501
kushwahaPrashant24 <146343202+kushwahaPrashant24@users.noreply.github.com>
2024-05-16 22:50:56 +0530

    added the main section

--- src/Componont/Home/Homepage/main.jsx
+++ src/Componont/Home/Homepage/main.jsx
@@ -2,7 +2,16 @@ import React from "react";
 
 function main() {
     return <>
-        <div className="h-60 bg-zinc -mt-8"></div>
+        <div className="h-60 bg-zinc -mt-8 flex justify-center flex-col" >
+            <h1 className="max-h-10  max-w-2xl sm:text-lg md:text-3xl bg-Amber divide-x-2 font-black text-center  border-2 mt-3 "> 30+ COMPANIES TRUST EditQuasar</h1>
+            <div className="flex justify-around">
+                <div className="w-36 min-h-36 mt-8 bg-Amber rounded-full"> </div>
+                <div className="w-36 min-h-36 mt-8 bg-Amber rounded-full"> </div>
+                <div className="w-36 min-h-36 mt-8 bg-Amber rounded-full"> </div>
+                <div className="w-36 min-h-36 mt-8 bg-Amber rounded-full"> </div>
+
+            </div>
+        </div>
     </>
 }
 
